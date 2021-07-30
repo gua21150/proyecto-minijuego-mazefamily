9411a5f7d015e3ce27ccdfc18c292f72cc9578be
gua21150 <gua21150@uvg.edu.gt>
2021-07-30 08:00:29 -0600

    Implementación mencionada anteriormente

--- MazeFamily/LlamadaRetos.java
+++ MazeFamily/LlamadaRetos.java
@@ -4,7 +4,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Esta clase solamente utiliza a sus objetos. Los objetos no contienen una acción en concreto.
  * 
  * @author Mariel Alejandra Guamuche Recinos 
+<<<<<<< Updated upstream
  * @version 1.3
+=======
+ * @version 1.4
+>>>>>>> Stashed changes
  */
 public class LlamadaRetos extends Actor
 {
