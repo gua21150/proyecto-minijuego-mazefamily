85bec00f15c6a08db61d8d4a68c27e1b13dfee3f
gua21150 <gua21150@uvg.edu.gt>
2021-07-30 07:59:11 -0600

    Error en ciclo while
    Se verificó que existía un error  por lo que se solucionó.

--- MazeFamily/LlamadaRetos.java
+++ MazeFamily/LlamadaRetos.java
@@ -4,11 +4,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * Esta clase solamente utiliza a sus objetos. Los objetos no contienen una acción en concreto.
  * 
  * @author Mariel Alejandra Guamuche Recinos 
-<<<<<<< Updated upstream
  * @version 1.3
-=======
- * @version 1.4
->>>>>>> Stashed changes
  */
 public class LlamadaRetos extends Actor
 {
--- MazeFamily/meta.java
+++ MazeFamily/meta.java
@@ -1,33 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
-<<<<<<< Updated upstream
  * Se utiliza su objeto para determinar cuando el jugador ha terminado el laberinto.
  * 
  * @author Mariel Alejandra Guamuche Recinos
  * @version 1.0
-=======
- * Esta clase indica cuando el carro ha llegado hasta la meta. Se utiliza más el objeto instanciado de esta clase.
- * 
- * @author Mariel Alejandra Guamuche Recinos 
- * @version 1.2
->>>>>>> Stashed changes
  */
 public class Meta extends Actor
-{    
+{
     /**
-<<<<<<< Updated upstream
      * En el constructor se le da tamaño al objeto para que no sea pequeño
      */
     
     public Meta()
     {
         getImage().scale(getImage().getWidth()/8, getImage().getHeight()/8);
-=======
-     * Constructor donde se modifica el tamaño del objeto meta
-     */
-    public Meta(){   
-        getImage().scale(getImage().getWidth()*2, getImage().getHeight()*2);
->>>>>>> Stashed changes
     }
 }
